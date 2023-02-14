d135cdcb6aa236681b5a08157d774bf06bd57274
Sahithi Pala <43152542+sahithipala1@users.noreply.github.com>
2023-02-14 16:38:01 +0100

    parameters in functions

---
+++ cake.py
@@ -0,0 +1,10 @@
+def whichCake(ingredient):
+    if ingredient == "chocolate":
+        print("Mmm, chocolate cake is amazing")
+    elif ingredient == "brocoli":
+        print("you what mate?!")
+    else:
+        print("yeah, that's great choice")
+
+
+whichCake("chocolate")
